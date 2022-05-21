993f2f5c17ce0306df39f8c140ca735cc5711ee7
Popuri Nithin <94154780+nithin-popuri7@users.noreply.github.com>
2022-05-21 21:10:52 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,48 @@
+import cv2
+import matplotlib.pyplot as plt
+image = cv2.imread("audi.jpg")
+
+# Read the Image and convert to grayscale
+
+grayImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# Use Global thresholding to segment the image
+
+ret,thresh = cv2.threshold(grayImage,100,200,cv2.THRESH_BINARY)
+ret1,thresh1 = cv2.threshold(grayImage,100,200,cv2.THRESH_BINARY_INV)
+ret2, thresh2 = cv2.threshold(grayImage,100,200,cv2.THRESH_TRUNC)
+ret3, thresh3 = cv2.threshold(grayImage,100,200,cv2.THRESH_TOZERO)
+ret4, thresh4 = cv2.threshold(grayImage,100,200,cv2.THRESH_TOZERO_INV)
+
+# Use Adaptive thresholding to segment the image
+
+th1 = cv2.adaptiveThreshold(grayImage,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+th2 = cv2.adaptiveThreshold(grayImage,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+titles = ["Original Image","Adaptive Mean Thresholding","Adaptive Gaussian Thresholding"]
+imgs = [grayImage,th1,th2]
+
+# Use Otsu's method to segment the image
+ret5,th3 = cv2.threshold(grayImage,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+# Display the results
+
+cv2.imshow("Original Image",image)
+cv2.imshow("Gray Image",grayImage)
+cv2.imshow("THRESH_BINARY",thresh)
+cv2.imshow("THRESH_BINARY_INV",thresh1)
+cv2.imshow("THRESH_TRUNC",thresh2)
+cv2.imshow("THRESH_TOZERO",thresh3)
+cv2.imshow("THRESH_TOZERO_INV",thresh4)
+
+for i in range(3):
+    plt.subplot(3,1,i+1)
+    plt.imshow(imgs[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+cv2.imshow("Otsu method",th3)
+cv2.waitKey(0)
+
+
